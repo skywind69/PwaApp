c2d29f1553ca070e40c3010fa8b67bdf662e200d
skywind69 <skywinddev@gmail.com>
2020-05-12 17:15:46 +0200

    feat: standard app starter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+import { DataService } from './data.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pwaTest';
+  title = 'app';
+
+  // update: boolean = false;
+  joke: any;
+  constructor(private data: DataService){}
+
+  // constructor(updates: SwUpdate, private data: DataService) {
+  //   updates.available.subscribe(event => {
+
+  //     //this.update = true;
+  //     updates.activateUpdate().then(() => document.location.reload());
+
+  //   })
+  // }
+
+  ngOnInit() {
+    this.data.gimmeJokes().subscribe(res => {
+      console.log(res);
+      this.joke = res;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DataService } from './data.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +12,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
